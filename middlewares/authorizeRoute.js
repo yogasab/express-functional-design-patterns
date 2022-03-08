ee407fe79ae8da5258d30d460936cb2ea903f4bb
Yoga Baskoro <baskoroyoga40@gmail.com>
2022-03-08 20:54:59 +0700

    Refactor to policy method middleware

--- middlewares/authorizeRoute.js
+++ middlewares/authorizeRoute.js
@@ -1,7 +1,15 @@
 const { sendErrorResponse } = require("../lib/response");
 
-const authorizeRoute = (req, res, next) => {
+// To check if the one who made changes is the one whose logged in
+const modifyPolicy = (req) => {
 	if (req.user.id !== req.id) {
+		return false;
+	}
+	return req.user.id === req.id;
+};
+
+const authorizeRoute = (req, res, next) => {
+	if (!modifyPolicy(req)) {
 		sendErrorResponse(res, 403, "failed", "You cannot perform this route");
 	}
 	next();
